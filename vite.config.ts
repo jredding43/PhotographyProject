fd45984512428ed230ba52d24e099f515488a0dc
Jack Redding <jredding2543@gmail.com>
2025-02-24 14:07:27 -0800

    adjust homepage name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Photography/", 
+  base: "/PhotographyProject/", 
   define: {
     "import.meta.env.VITE_API_BASE_URL": JSON.stringify(process.env.VITE_API_BASE_URL),
   }  
